566291acfeb8f1875f888fddf6f7a942995e50e2
udemba Godswill <godswill.udemna.pg88009@unn.edu.ng>
2023-02-07 10:23:12 +0100

    third commit

--- starter/script.js
+++ starter/script.js
@@ -1,5 +1,12 @@
 //Importing Module
 
-import { addToCart } from './shoppingCart.js';
+//import { addToCart, totalPrice, totalQuantity } from './shoppingCart.js';
+// addToCart('bread', 5);
+// console.log(totalPrice, totalQuantity);
+
 console.log('importing Module');
-addToCart('bread', 5);
+//to import everything one
+
+import * as shoppingCart from './shoppingCart.js';
+shoppingCart.addToCart('bread', 5);
+console.log(shoppingCart.totalPrice);
--- starter/shoppingCart.js
+++ starter/shoppingCart.js
@@ -9,3 +9,10 @@ export const addToCart = function (product, quantity) {
   cart.push({ product, quantity });
   console.log(`${quantity} ${product} added to cart`);
 };
+
+const totalPrice = 276;
+const totalQuantity = 24;
+export { totalPrice, totalQuantity };
+
+//default export
+export default
