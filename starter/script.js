b69592569225b1081599f5ea27a06226f322e3fe
udemba Godswill <godswill.udemna.pg88009@unn.edu.ng>
2023-02-07 13:06:17 +0100

    fouth commit

--- starter/script.js
+++ starter/script.js
@@ -7,6 +7,35 @@
 console.log('importing Module');
 //to import everything one
 
-import * as shoppingCart from './shoppingCart.js';
-shoppingCart.addToCart('bread', 5);
-console.log(shoppingCart.totalPrice);
+// import * as shoppingCart from './shoppingCart.js';
+// shoppingCart.addToCart('bread', 5);
+// console.log(shoppingCart.totalPrice);
+
+import add, { cart } from './shoppingCart.js';
+add('pizza', 2);
+add('bread', 6);
+add('Apple', 8);
+
+console.log(cart);
+
+//TOP LEVEL AWAIT
+// console.log('start fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('fetch something');
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+
+  //get the last title and body text
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+//store the get element in to a variable
+const lastPost = getLastPost();
+console.log(lastPost); //at this point it can only return a promise
+
+//we deploy a high level await
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
--- starter/shoppingCart.js
+++ starter/shoppingCart.js
@@ -2,8 +2,14 @@
 
 console.log('Exporting module');
 
+//Blocking code
+// console.log('start fetching users');
+// await fetch('https://jsonplaceholder.typicode.com/users');
+// console.log('finished fetching user');
+//
+
 const shippingCost = 10;
-const cart = [];
+export const cart = [];
 
 export const addToCart = function (product, quantity) {
   cart.push({ product, quantity });
@@ -14,5 +20,8 @@ const totalPrice = 276;
 const totalQuantity = 24;
 export { totalPrice, totalQuantity };
 
-//default export
-export default
+//default export is used when we want to export one thing per module
+export default function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+}
